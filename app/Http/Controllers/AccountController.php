b2bb3045404a710c2e250a4828bafb347439effc
Roberto Vecilla <roberto.vecilla@gmail.com>
2017-11-28 07:48:07 +0000

    json web token auth first implementation at signup

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AccountController extends Controller
 {
-    //
+	// Sign up
+    public function index(Request $request)
+	{
+
+		try {
+			$user = User::create([
+				'name' => $request->name,
+				'email' => $request->email,
+				'password' => bcrypt($request->password),
+			]);
+		} catch(\Exception $e) {
+			// User already exists
+			return response()->json($e->errorInfo, 409);
+		}
+
+
+		$token = JWTAuth::fromUser($user);
+
+		return response()->json(compact('token'));
+		//return response()->json($user, 201);
+	}
 }
