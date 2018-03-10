6f072decd80750f104a0c5cfed8d6e1a0c41032b
Roberto Vecilla <roberto.vecilla@gmail.com>
2018-03-10 19:40:13 +0000

    calculateTotals implemented + receipt creation

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Receipt;
+use App\ReceiptDetail;
 use App\User;
 use Illuminate\Http\Request;
 
 class ReceiptController extends Controller
 {
+
+    private $total = 0;
+    private $subtotal = 0;
+
     /**
      * Display a generic listing of receipts.
      * Returns only receipts attached to a particular user from their JWT.
@@ -17,44 +22,94 @@ class ReceiptController extends Controller
     {   
         $user = auth()->user();
         $receipts = User::find($user->id)->receipts;
-
+        
         foreach ($receipts as $key => $receipt) {
-            // Just getting the name here
-            $receipts[$key]['retailer_name'] = Receipt::find($receipt['retailer_id'])->retailer->name;
-        }
-
-        return response()->json($receipts, 201);
-    }
-
-     /**
-     * Display specific list of receipts based on the web-client requirement.
-     * Returns only receipts attached to a particular user from their JWT.
-     * @return \Illuminate\Http\Response
-     */
-    public function getUserReceipts()
-    {
-        $user = auth()->user();
-        $receipts = User::find($user->id)->receipts->toArray();
-
-        foreach ($receipts as $key => $receipt) {
-            // Just getting the name here
-            $receipts[$key]['retailer_name'] = Receipt::find($receipt['retailer_id'])->retailer->name;
+            $receipts[$key]['retailer_name'] = Receipt::find($receipt['id'])->retailer->name;
+            $receipts[$key]['subtotal'] = Receipt::find($receipt['id'])->receiptDetail->subtotal;
         }
 
         return response()->json($receipts, 201);
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
+     * Creates a new receipt record.
+     * Structure example:
+     * 
+     *  [
+     *      {
+     *          "user": 1,
+     *          "retailer": 2,
+     *          "items":[
+     *                   {
+     *                       "name": "item1",
+     *                       "price": 12,
+     *                       "quantity": 1,
+     *                       "serial_no": 76936078
+     *                   },
+     *                   {
+     *                       "name": "item2",
+     *                       "price": 673.43,
+     *                       "quantity": 1,
+     *                       "serial_no": 43158526
+     *                   },
+     *                   {       
+     *                       "name": "item3",
+     *                       "price": 193.71,
+     *                       "quantity": 5,
+     *                       "serial_no": 32735161341820
+     *                   }
+     *                  ],
+     *          "payment": "visa",
+     *          "vat": 21
+     *    }
+     *  ]
+     * 
+     * The total is calculated iterating over the array of item objects and 
+     * dynamically adding item prices to the total.
+     * 
+     * The subtotal is calculated by applyting the VAT over the total.
+     * 
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        
+        $this->calculateTotals($request->items, $request->vat);
+
 		// Creates a receipt
-		$receipt = Receipt::create($request->all());
-        return response()->json($receipt, 201);
+		$receipt = Receipt::create([
+            'user_id' => $request->user,
+            'retailer_id' => $request->retailer
+        ]);
+        
+        // Creates its detail
+        $detail = ReceiptDetail::create([
+            'receipt_id' => $receipt->id,
+            'items' => $request->items,
+            'total' => round($this->total, 2),
+            'subtotal' => round($this->subtotal, 2),
+            'payment_method' => $request->payment,
+            'VAT' => $request->vat
+        ]);
+
+        return response()->json('Created successfully', 201);
+    }
+
+    /**
+     * Calculates receipt totals given an array of items and VAT percentage.
+     * @return \Illuminate\Http\Response
+     */
+    private function calculateTotals($items, $vat)
+    {
+        $items = json_decode($items);
+
+        foreach ($items as $key => $item) {
+            $this->total += $item->price;
+        }
+        foreach ($items as $key => $item) {
+            $this->subtotal = $this->total + ($vat / 100) * $this->total;
+        }
     }
 
      /**
@@ -66,45 +121,27 @@ class ReceiptController extends Controller
     {
         $detail['retailer'] = Receipt::find($receipt->id)->retailer;
         $detail['receipt'] = $receipt->receiptDetail;
-        // receipt foother information
+        // receipt footer information
         // $detail['footer_info'] = $receipt->footer; or smnth like that
 
         return response()->json($detail, 201);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Receipt  $receipt
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Receipt $receipt)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Receipt  $receipt
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Receipt $receipt)
-    {
-		$receipt->update($request->all());
-        return response()->json($receipt, 200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
+     * Removes Receipt and ReceiptDetail.
      *
      * @param  \App\Receipt  $receipt
      * @return \Illuminate\Http\Response
      */
     public function destroy(Receipt $receipt)
     {
-		$receipt->delete();
+        // Find its detail via Receipt ID
+        $detail = ReceiptDetail::find($receipt->id);
+
+        // Deletes both receipt and detail
+        $receipt->delete();
+        $detail->delete();
+
         return response()->json(null, 204);
     }
 }
